481cd2a29600c16d7d6d440cb0e338d3a66c818c
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-12-07 08:50:06 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+let tira2 = neopixel.create(DigitalPin.P1, 300, NeoPixelMode.RGB)
 strip.clear()
+tira2.clear()
 basic.forever(function () {
-    strip.setPixelColor(11, neopixel.rgb(255, 70, 50))
+    strip.setPixelColor(10, neopixel.rgb(255, 10, 0))
     strip.setBrightness(randint(25, 255))
     strip.show()
     basic.pause(randint(0, 150))
-    strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Black))
     strip.show()
     basic.pause(randint(0, 150))
+})
+basic.forever(function () {
+	
 })
